5b1b8e6aa3c2efeaced2bc799443342ef492ffd1
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2015-02-06 14:26:31 +0100

    everything working - hover too

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+  $('#fr_54450885d775ef4cd100002b').hover(function(){
+    $("#cohomerButton").hide();
+    $('#cohomerButtonTranslation').show();
+  }, function(){
+    $('#cohomerButtonTranslation').hide();
+    $('#cohomerButton').show();
+  });
+  $('#fr_54450885d775ef4cd100002b3').hover(function(){
+    $("#coworkerButton").hide();
+    $('#coworkerButtonTranslation').show();
+  }, function(){
+    $('#coworkerButtonTranslation').hide();
+    $('#coworkerButton').show();
+  });
+});
